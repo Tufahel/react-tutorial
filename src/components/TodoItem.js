e3bd13050723c7d04d0b03bc9a14ee21383fdf5c
Tufahel <tufahel96@gmail.com>
2022-03-22 17:12:06 +0600

    edit todos in react

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,7 +3,19 @@ import React from 'react';
 import styles from "./TodoItem.module.css"
 
 class TodoItem extends React.Component {
-  
+  state = {
+    editing: false,
+  }
+  handleEditing = () => {
+    this.setState({
+      editing: true,
+    })
+  }
+  handleUpdatedDone = event => {
+    if (event.key === "Enter") {
+      this.setState({ editing: false })
+    }
+  }
   render() {
     const completedStyle = {
       fontStyle: "italic",
@@ -11,9 +23,30 @@ class TodoItem extends React.Component {
       opacity: 0.4,
       textDecoration: "line-through",
     }
-    const { completed, id, title } = this.props.todo
+    const { completed, id, title } = this.props.todo;
+    let viewMode = {}
+    let editMode = {}
+
+    if (this.state.editing) {
+      viewMode.display = "none"
+    } else {
+      editMode.display = "none"
+    }
     return (
       <li className={styles.item}>
+        <div onDoubleClick={this.handleEditing} style={viewMode}>
+          ...
+        </div>
+        <input
+          type="text"
+          style={editMode}
+          className={styles.textInput}
+          value={title}
+          onChange={e => {
+            this.props.setUpdate(e.target.value, id)
+          }}
+          onKeyDown={this.handleUpdatedDone}
+        />
         <input
           className={styles.checkbox}
           type="checkbox"
